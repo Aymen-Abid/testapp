d1c5eac0260cfb5f4c1a6fbf127be7083afa30c5
FoxTN1998 <84271100+FoxTN1998@users.noreply.github.com>
2021-07-28 11:25:48 +0100

    Update App.js
    a

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ya dimax ya miboun
+          ya dimax ya miboun w yatik 3asba mel fou9 <br>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
